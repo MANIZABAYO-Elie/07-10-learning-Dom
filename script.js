3047086327e77c68a8ccd3aeb9a65dab0ead565e
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-10 23:26:03 +0200

    file committed

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const submitButton = document.getElementById('submit-button');
 const sportsItems = document.querySelector('#sports');
+const thanks   = document.querySelector('#thanks')
 submitButton.addEventListener('click',function(){
     //console.log('hello world');
     changeColor();
+    display()
     
     
 })
 function changeColor(){
     sportsItems.style.size = "blue";
+
+}
+function display(){
+    thanks.innerHTML = "thanks for reaching with us"
 }
 
 
