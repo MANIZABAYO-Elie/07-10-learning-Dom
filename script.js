de03414685f2a451f447050d20be69072eaea73b
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-10 23:13:02 +0200

    file committed

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const submitButton = document.getElementById('submit-button');
+const sports = document.querySelectorAll('#sports');
 submitButton.addEventListener('click',function(){
     console.log('hello world');
+    changeColor();
+    
     
 })
+function changeColor(){
+    sports.style.color = "blue";
+}
+
+
 
